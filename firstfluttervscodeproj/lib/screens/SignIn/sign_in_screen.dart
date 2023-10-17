5045cb2dc637261ee5785d7437249edac0a6aedb
iMac <imac@iMacs-iMac.local>
2023-10-17 17:36:21 +0530

    Add back button functionality and action click

--- firstfluttervscodeproj/lib/screens/SignIn/sign_in_screen.dart
+++ firstfluttervscodeproj/lib/screens/SignIn/sign_in_screen.dart
@@ -3,14 +3,31 @@
 import 'package:firstfluttervscodeproj/screens/SignIn/components/body.dart';
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   static String routeName = "/sign_in";
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+
+
+@override
+  void initState() {
+    FocusManager.instance.primaryFocus?.hasFocus;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Sign In"),
+        leading: InkWell(onTap:(){
+            FocusManager.instance.primaryFocus?.unfocus();
+            Navigator.of(context).pop();
+        },child: Icon(Icons.arrow_back)),
       ),
       body: BodyLogin(),
     );
